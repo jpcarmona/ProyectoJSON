7b53a926871ef77f71cc477197484bd73f45d615
jpcarmona <jpcarmona92@gmail.com>
2018-03-10 17:22:36 +0100

    Avance4 funciones y proyecto

--- funciones.py
+++ funciones.py
@@ -6,10 +6,44 @@ def borrado_total(datos):
 	for i in range(3):
 		datos.pop(0)
 
+def provincias(datos,provincia):
+	""" Devuelve la provincia del fichero
+	dado una provincia si existe en el fichero,
+	quitando las tildes """
+	lista_prov=[]
+	lista_tildes=["á","é","í","ó","ú"]
+	lista_vocal=["a","e","i","o","u"]
+	for dato in datos:
+		sex=dato["MetaData"][1]["Nombre"]
+		prov=dato["MetaData"][0]["Nombre"]
+		if sex=="Total":
+			lista_prov.append(prov)
+	for prov in lista_prov:
+		prov1=prov.lower()
+		prov2=""
+		for caracter in prov1:
+			if caracter in lista_tildes:
+				for tilde,vocal in zip(lista_tildes,lista_vocal):
+					if caracter==tilde:
+						prov2+=vocal
+			else:
+				prov2+=caracter
+		if provincia in prov2:
+			return prov
+	return False
+
+def años(datos):
+	""" Devuelve los años
+	comprendidos en el fichero """
+	lista_años=[]
+	for año in datos[0]["Data"]:
+		lista_años.append(año["Anyo"])
+	return lista_años
+
 def opcion1(datos,cifra,años):
 	dict1={}
 	for año in años:
-		dict1[año]=[]	
+		dict1[año]=[]
 	for dato in datos:
 		if dato["MetaData"][1]["Nombre"]=="Total":
 			for año in dato["Data"]:
@@ -37,11 +71,11 @@ def opcion2(datos,sexo):
 				dict1[año["Anyo"]][provincia]={}
 		else:
 			for año in dato["Data"]:
-				poblacion=año["Valor"]			
+				poblacion=año["Valor"]
 				if sex=="Hombres":
 					dict1[año["Anyo"]][provincia][sex]=poblacion
 				if sex=="Mujeres":
-					dict1[año["Anyo"]][provincia][sex]=poblacion		
+					dict1[año["Anyo"]][provincia][sex]=poblacion
 	for key in dict1.keys():
 		print("Año {}".format(key))
 		num=0
@@ -49,7 +83,7 @@ def opcion2(datos,sexo):
 			for sex in dict1[key][provincia].keys():
 				pob=dict1[key][provincia][sex]
 				if sex==sexo:
-					pob1=pob										
+					pob1=pob
 				else:
 					pob2=pob
 			if pob1>pob2:
@@ -61,7 +95,7 @@ def opcion3(rango1,rango2,año1,datos):
 	dict1={}
 	for dato in datos:
 		provincia=dato["MetaData"][0]["Nombre"]
-		sex=dato["MetaData"][1]["Nombre"]		
+		sex=dato["MetaData"][1]["Nombre"]
 		for año in dato["Data"]:
 			if año["Anyo"]!=año1:
 				continue
@@ -75,15 +109,15 @@ def opcion3(rango1,rango2,año1,datos):
 					opcion=False
 			if opcion:
 				dict1[provincia][sex]=total
-	for provincia in dict1.keys():		
+	for provincia in dict1.keys():
 		for sex in dict1[provincia].keys():
 			pob=dict1[provincia][sex]
 			if sex=="Hombres":
-				pob1=pob										
+				pob1=pob
 			if sex=="Mujeres":
 				pob2=pob
 			if sex=="Total":
-					pob3=pob
+				pob3=pob
 		print("{} tiene {} habitantes.".format(provincia,int(pob3)))
 		if pob1>pob2:
 			print("Hay mas hombres que mujeres en",provincia)
@@ -93,12 +127,48 @@ def opcion3(rango1,rango2,año1,datos):
 			#seguramente sea imposible...
 			print("Hay las mismas mujeres que hombres en",provincia)
 		print("")
-				
 
-def opcion4(provincia,sexo,año,datos):
 
+def opcion4(provincia,sexo,año1,datos):
+	for dato in datos:
+		prov=dato["MetaData"][0]["Nombre"]
+		sex=dato["MetaData"][1]["Nombre"]
+		if prov==provincia and sex==sexo:
+			for año in dato["Data"]:
+				if año["Anyo"]==año1:
+					total=año["Valor"]
+					print("La población de {} en el {} de {} es de {} habitantes\n".format(provincia,año1,sexo,int(total)))
+					break
+			break
 
 
 def opcion5(sexo,datos):
-	
+	dict1={}
+	for año in datos[0]["Data"]:
+		dict1[año["Anyo"]]={}
+	for dato in datos:
+		sex=dato["MetaData"][1]["Nombre"]
+		provincia=dato["MetaData"][0]["Nombre"]
+		if sex=="Total" :
+			for año in dato["Data"]:
+				dict1[año["Anyo"]][provincia]={}
+		else:
+			for año in dato["Data"]:
+				poblacion=año["Valor"]
+				if sex=="Hombres":
+					dict1[año["Anyo"]][provincia][sex]=poblacion
+				if sex=="Mujeres":
+					dict1[año["Anyo"]][provincia][sex]=poblacion
+	for key in dict1.keys():
+		print("Año {}\n".format(key))
+		for provincia in dict1[key].keys():
+			for sex in dict1[key][provincia].keys():
+				pob=dict1[key][provincia][sex]
+				if sex==sexo:
+					pob1=pob
+				else:
+					pob2=pob
+			if pob1>pob2:
+				print("{} --> {} {}".format(provincia,int(pob1),sexo))
+		print("\n")
 
--- proyectoJSON.py
+++ proyectoJSON.py
@@ -1,13 +1,15 @@
 import json
-from funciones import borrado_total,opcion1,opcion2,opcion3,opcion4,opcion5
+from funciones import borrado_total,opcion1,opcion2,opcion3,opcion4,opcion5,provincias,años
 
 with open("poblacion.json") as fichero:
 	datos=json.load(fichero)
 
 borrado_total(datos)
 
+listaaños=años(datos)
+
 while True:
-	print("""Elija una opción de las siguientes 5 (\"0\" para salir)\n
+	print("""Escriba una opción de las siguientes 5 (\"0\" para salir)\n
 1.Provincias que tienen mas de una cifra de poblacion en los años dados.
 2.Numero de provincias que tengan mas población de un sexo por años.
 3.Muestra las provincias que esten en un rango de población y año y muestra el sexo 
@@ -16,7 +18,7 @@ con mayor población en cada provincia.
 5.Muestra las provincias por año en el que la población mayor es de un sexo.\n""")
 	opcion=input("Opcion: ")
 	if opcion not in ["0","1","2","3","4","5"]:
-		print("Por favor elija una opcion correcta\n")
+		print("Por favor Escriba una opcion correcta\n")
 		continue
 	if opcion=="0":
 		break
@@ -27,20 +29,23 @@ con mayor población en cada provincia.
 			if cifra==0:
 				break			
 			años=[]
-			num=0
+			num=1
 			print("Escriba los años deseados comprendidos entre 1998-2017 o sea 1996")
-			while True:
-				num+=1
+			while True:				
 				año=int(input("Año {} (0) para continuar: ".format(num)))
-				if año !=0:
-					años.append(año)
-				else:
+				if año==0:
 					break
+				if año not in listaaños:
+					print("No es un año valido")
+					continue
+				else:
+					años.append(año)
+					num+=1					
 			print("")
 			opcion1(datos,cifra,años)
 	if opcion=="2":
 		while True:
-			sexo=input("Elija un sexo (h/Hombre m/mujer) (0 para volver):\n")
+			sexo=input("Escriba un sexo (h/Hombre m/mujer) (0 para volver):\n")
 			print("")
 			if sexo=="0":
 				break
@@ -51,24 +56,61 @@ con mayor población en cada provincia.
 			opcion2(datos,sexo)
 	if opcion=="3":
 		while True:
-			año=int(input("Elija un año que esté comprendido entre 1998-2017 o sea 1996 (0 para volver):\n"))
+			año=int(input("Escriba un año que esté comprendido entre 1998-2017 o sea 1996 (0 para volver):\n"))
 			print("")
 			if año==0:
 				break
-			rango1=int(input("Elija el rango1 de población:\n"))
+			if año not in listaaños:
+				print("No es un año valido")
+				continue
+			rango1=int(input("Escriba el rango1 de población:\n"))
 			while True:
-				rango2=int(input("Elija el rango2 de población:\n"))
+				rango2=int(input("Escriba el rango2 de población:\n"))
 				if rango2<rango1:
-					print("Por favor elija un rango2 mayor al de rango1")
+					print("Por favor Escriba un rango2 mayor al de rango1")
 				else:
 					break
 			print("")
 			opcion3(rango1,rango2,año,datos)
 	if opcion=="4":
 		while True:
-
+			año=int(input("Escriba un año que esté comprendido entre 1998-2017 o sea 1996 (0 para volver):\n"))
+			print("")
+			if año==0:
+				break
+			if año not in listaaños:
+				print("No es un año valido")
+				continue
+			while True:
+				provincia=input("Escriba una provincia de España (0 para volver):\n")
+				print("")
+				if provincia=="0":
+					break				
+				if not provincias(datos,provincia):
+					print("Provincia Incorrecta")
+					continue
+				else:
+					provincia=provincias(datos,provincia)
+					break
+			if provincia=="0":
+				break
+			sexo=input("Escriba un sexo (h/Hombre m/mujer) (0 para volver):\n")
+			print("")
+			if sexo=="0":
+				break
+			if sexo=="h":
+				sexo="Hombres"
+			if sexo=="m":
+				sexo="Mujeres"
 			opcion4(provincia,sexo,año,datos)
 	if opcion=="5":
 		while True:
-
+			sexo=input("Escriba un sexo (h/Hombre m/mujer) (0 para volver):\n")
+			print("")
+			if sexo=="0":
+				break
+			if sexo=="h":
+				sexo="Hombres"
+			if sexo=="m":
+				sexo="Mujeres"			
 			opcion5(sexo,datos)
